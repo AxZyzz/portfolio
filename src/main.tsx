d187727b917a325488856805b01b59e1a204b097
AxZyzz <amanxavierwork@gmail.com>
2025-04-22 12:41:41 +0530

    Fix router

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,6 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
 import './index.css'
-import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 
 const root = document.getElementById('root')
 if (root) {
@@ -12,21 +11,8 @@ if (root) {
   root.style.padding = '0'
 }
 
-const router = createBrowserRouter(
-  [
-    {
-      path: '/',
-      element: <App />
-    }
-    // Your other routes here
-  ],
-  {
-    basename: '/portfolio' // Set to your GitHub repo name (without trailing slash)
-  }
-)
-
 ReactDOM.createRoot(root!).render(
   <React.StrictMode>
-    <RouterProvider router={router} />
+    <App />
   </React.StrictMode>,
 )
